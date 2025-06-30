e0bd17de1bf973611af3a48f30da96c86acac7a5
zttrrrr <95978695+zttrrrr@users.noreply.github.com>
2025-06-30 08:25:57 -0300

    vinculo

--- TP SITIO VENTAS/TP-SitioVentas/src/App.jsx
+++ TP SITIO VENTAS/TP-SitioVentas/src/App.jsx
@@ -5,21 +5,24 @@ import QuienesSomos from './pages/QuienesSomos';
 import Productos from './pages/Productos';
 import ProductoDetalle from './pages/ProductoDetalle';
 import Contacto from './pages/Contacto';
+import './App.css'; // 🔥 Importa los estilos nuevos acá
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<Home />} />
-          <Route path="quienes-somos" element={<QuienesSomos />} />
-          <Route path="productos" element={<Productos />} />
-          <Route path="productos/marca/:idMarca" element={<Productos />} />
-          <Route path="producto/:id" element={<ProductoDetalle />} />
-          <Route path="contacto" element={<Contacto />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <div id="root">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            <Route path="quienes-somos" element={<QuienesSomos />} />
+            <Route path="productos" element={<Productos />} />
+            <Route path="productos/marca/:idMarca" element={<Productos />} />
+            <Route path="producto/:id" element={<ProductoDetalle />} />
+            <Route path="contacto" element={<Contacto />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
